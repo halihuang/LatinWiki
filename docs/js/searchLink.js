5c23a07c6fb299a40e00e492957875d35f7977e7
d d <haliw@LAPTOP-AEUTU78G.mit.edu>
2021-08-10 14:04:03 -0400

    fixed search clicked link

--- docs/js/searchLink.js
+++ docs/js/searchLink.js
@@ -8,17 +8,15 @@ linkSearcher.searchLink = function(){
   {
     let clickedHTML = event.target.innerHTML;
     if(clickedHTML != "Translate" && clickedHTML != "Saved" && clickedHTML != "History" && clickedHTML != "About" && clickedHTML != "Forget"){
-      if(navigation.title != "About"){
-        main.input = clickedHTML.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-        previousSetting = main.latinToEng;
-        main.latinToEng = true;
+      main.input = clickedHTML.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+      previousSetting = main.latinToEng;
+      main.latinToEng = true;
+      await main.searchLatin();
+      if(main.errored = true && main.translation == ""){
+        main.latinToEng = false;
         await main.searchLatin();
-        if(main.errored = true && main.translation == ""){
-          main.latinToEng = false;
-          await main.searchLatin();
-        }
-        main.latinToEng = previousSetting;
       }
+      main.latinToEng = previousSetting;
     }
   });
 };
